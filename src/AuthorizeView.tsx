da3f3d817f7eb17f6cdeeb97c9078581cbbace05
Benjamin Bergstein <bennyjbergstein+github@gmail.com>
2022-01-30 11:20:29 -0800

    Fix auth bug.

--- src/AuthorizeView.tsx
+++ src/AuthorizeView.tsx
@@ -4,37 +4,51 @@ import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { getAuthUrl } from "./spotify";
 import Logo from "./Logo";
 import Copyright from "./Copyright";
+import { useMe, useView } from "./hooks";
+import { useEffect } from "react";
 
-const AuthorizeView: React.FC = () => (
-  <Flex
-    p={3}
-    flex={1}
-    direction="row"
-    align="center"
-    width="100%"
-    justify="center"
-  >
-    <VStack spacing="30px" align="right" maxWidth="600px">
-      <Logo width="300px" height="72px" />
+const AuthorizeView: React.FC = () => {
+  const me = useMe();
+  const [_view, setView] = useView();
 
-      <Heading color="gray.800" size="lg">
-        Meander through new music
-      </Heading>
-      <Heading color="gray.600" size="md">
-        Add spontaneity to your queue and playlists.
-      </Heading>
-      <Text color="gray.800" fontSize="sm">
-        Zero in on what you're looking for by tweaking dimensions such as energy
-        / danceability / popularity).
-      </Text>
-      <a href={getAuthUrl()}>
-        <Button rightIcon={<ExternalLinkIcon />} colorScheme="pink">
-          Connect Spotify
-        </Button>
-      </a>
-    </VStack>
-    <Copyright layout="fixed" />
-  </Flex>
-);
+  useEffect(() => {
+    if (me?.data?.id) {
+      console.log("Asdfa", _view);
+      setView("tune");
+    }
+  }, [me?.data?.id]);
+
+  return (
+    <Flex
+      p={3}
+      flex={1}
+      direction="row"
+      align="center"
+      width="100%"
+      justify="center"
+    >
+      <VStack spacing="30px" align="right" maxWidth="600px">
+        <Logo width="300px" height="72px" />
+
+        <Heading color="gray.800" size="lg">
+          Meander through new music
+        </Heading>
+        <Heading color="gray.600" size="md">
+          Add spontaneity to your queue and playlists.
+        </Heading>
+        <Text color="gray.800" fontSize="sm">
+          Zero in on what you're looking for by tweaking dimensions such as
+          energy / danceability / popularity).
+        </Text>
+        <a href={getAuthUrl()}>
+          <Button rightIcon={<ExternalLinkIcon />} colorScheme="pink">
+            Connect Spotify
+          </Button>
+        </a>
+      </VStack>
+      <Copyright layout="fixed" />
+    </Flex>
+  );
+};
 
 export default AuthorizeView;
